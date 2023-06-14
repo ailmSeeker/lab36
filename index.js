f8db860567b172acb3c40b1fceed6fe9d58fe23c
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-14 11:15:17 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+  return Math.abs(someValue - 42);  
+}
+
+function distanceFromHqInFeet(someValue){
+  return distanceFromHqInBlocks(someValue) * 264; 
+}
+
+function distanceTravelledInFeet(start, end){
+  let diff = Math.abs(end-start) * 264;
+  return diff;
+}
+
+function calculatesFarePrice(start, end){
+  let far = distanceTravelledInFeet(start, end)
+  let fare = 0;
+ 
+  if(far <= 400){
+    return 0; 
+  }
+
+  if(far > 400 && far < 2000){
+    fare = (far - 400) * 0.02;
+  }
+
+  if(far > 2000 && far < 2500){
+    return 25; 
+  }
+  if(far > 2500){
+    return "cannot travel that far"
+  }
+  return fare;
+
+}
